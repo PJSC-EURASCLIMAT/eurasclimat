d09ee19332d0e7fb6a220668ee06980cfaf6e56b
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2011-04-25 16:46:01 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@18 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate.sql
@@ -0,0 +1,3 @@
+INSERT INTO `acl_resources` (`name`, `title`) VALUES ('catalog', 'Каталог');
+INSERT INTO `acl_permissions` (`role_id`, `resource_id`, `privilege_id`) VALUES ('1', '2', '1');
+INSERT INTO `acl_permissions` (`role_id`, `resource_id`, `privilege_id`) VALUES ('1', '2', '3');

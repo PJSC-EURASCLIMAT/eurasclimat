17924d440dc14194409867b16441d2d5556ebe1c
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2011-04-26 15:08:26 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@19 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ library/OSDN/Status.php
@@ -0,0 +1,24 @@
+<?php
+
+class OSDN_Status extends OSDN_Response_Status_Storage_Abstract
+{
+    /**
+     * Module code
+     * @var int
+     */
+    protected $_moduleCode = 1;
+
+    /**
+     * Module name
+     * @var int
+     */
+    protected $_moduleName = 'OSDN';
+
+    /**
+     * Description storage
+     * @var array
+     */
+    protected $_storage = array(
+
+    );
+}

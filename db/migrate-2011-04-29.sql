a885c3cd9c00f3848ec3a0e35623bc160809eb8d
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2011-09-30 17:52:08 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@42 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate-2011-04-29.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `catalog_items` CHANGE `price` `price` DOUBLE(10,2) NULL; 
-ALTER TABLE `catalog_items` CHANGE `type_id` `type_id` INT(10) UNSIGNED NULL;
-ALTER TABLE `catalog_items` CHANGE `chapter_id` `chapter_id` INT(10) UNSIGNED NULL;  
-ALTER TABLE `catalog_items` ADD `cold` INT NULL, ADD `warm` INT NULL, ADD `power` DOUBLE(10,3) NULL; 

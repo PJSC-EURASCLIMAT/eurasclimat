7e71ff0cf6c97fca660b5a1588d5fd9142b69faf
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2012-03-11 01:06:24 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@122 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/start.js
@@ -0,0 +1,13 @@
+/*
+Ext.Loader.setPath({
+    'Ext.ux.desktop': 'js',
+    MyDesktop: ''
+});
+
+Ext.require('MyDesktop.App');
+
+var myDesktopApp;
+Ext.onReady(function () {
+    myDesktopApp = new MyDesktop.App();
+});
+*/

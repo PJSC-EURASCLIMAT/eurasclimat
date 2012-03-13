bea00a2d0d09ca5851f88d9c6221e840a08fdc94
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2012-03-13 03:58:45 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@167 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/MyDesktop/controller/Users.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EC.Layout.controller.Users', {
-    extend: 'Ext.app.Controller',
-
-    init: function() {
-        console.log('Initialized Users! This happens before the Application launch function is called');
-    }
-});

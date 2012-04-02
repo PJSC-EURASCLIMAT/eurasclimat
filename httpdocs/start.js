762eff6f007966583a8e4679b41e69de51c422e5
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2012-04-02 12:11:25 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@206 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/start.js
+++ httpdocs/start.js
@@ -13,9 +13,22 @@ Ext.application({
     appFolder: 'app',
     controllers: ['Main'],
     launch: function() {
+        var vpMask = new Ext.LoadMask(Ext.getBody(), {msg:'Загрузка...'}); 
+        vpMask.show();
+        
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
-            items: [this.getView('Layout').create()]
+            items: [this.getView('Layout').create()],
+            listeners: {
+                render: function() {
+                    vpMask.destroy();
+                    vpMask = new Ext.LoadMask(Ext.getBody(), {msg:'Инициализация...'});
+                    vpMask.show();
+                },
+                afterLayout: function() {
+                    vpMask.destroy();
+                }
+            }
         });
     }
 });

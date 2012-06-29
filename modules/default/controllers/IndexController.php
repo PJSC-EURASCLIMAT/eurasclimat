7a47a3dca296f0c372ef1c1d147852d8b2273cce
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2012-06-29 22:35:41 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@274 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- modules/default/controllers/IndexController.php
+++ modules/default/controllers/IndexController.php
@@ -106,6 +106,31 @@ class IndexController extends Xend_Controller_Action
         header('Location: /');
     }
 
+    public function getPermissionsAction()
+    {
+        $this->disableLayout(true);
+
+        $acl = Xend_Accounts_Prototype::getAcl();
+        $aclCollection = array();
+        if (!empty($acl)) {
+            $aclCollection = (object) $acl->toArray();
+        };
+        $this->view->acl = $aclCollection;
+
+        $resourceCollection = array();
+        $resource = new Xend_Acl_Resource();
+        $response = $resource->fetchAll();
+        if ($response->isSuccess()) {
+            foreach ($response->rows as $row) {
+                $resourceCollection[] = array($row['id'], strtolower($row['name']), $row['parent_id']);
+            }
+        }
+
+        $this->view->resources = $resourceCollection;
+        $privilege = Xend_Acl_Privilege::fetchAll();
+        $this->view->privileges = (object) $privilege;
+    }
+
     public function getCountriesAction()
     {
         $countries = Zend_Locale::getCountryTranslationList();

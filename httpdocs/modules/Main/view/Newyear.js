f8bdc71229f9a1318586975f63bda93f87e2bd5a
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2012-12-23 16:11:11 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@339 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Main/view/Newyear.js
@@ -0,0 +1,29 @@
+Ext.define('EC.Main.view.News', {
+
+    extend: 'Ext.panel.Panel',
+
+    autoScroll: true,
+    
+    layout: 'fit',
+    
+    html: '<div style="text-align: justify; padding: 10px;">' +
+    
+        '<h1>12.12.2012 - Заголовок</h1><br/>' + 
+        '<p style="text-indent: 2em;">Lorem ipsum dolor sit amet, ' +
+        'consectetur adipiscing elit. Nam risus arcu, lobortis sit ' +
+        'amet aliquam vitae, mollis id lacus. Sed eget nibh nulla. </p><br/><br/>' +
+        
+        '<h1>12.12.2012 - Заголовок</h1><br/>' +
+        '<p style="text-indent: 2em;">Vivamus vulputate, sem id pharetra porta, ' +
+        'turpis nunc elementum ipsum, quis varius elit felis id nisl. ' +
+        'Quisque interdum purus ut eros sodales egestas. Suspendisse suscipit ' +
+        'urna rutrum est sagittis quis tempor metus consequat.</p><br/><br/>' +
+        
+        '<h1>12.12.2012 - Заголовок</h1><br/>' +
+        '<p style="text-indent: 2em;">Nullam mi lectus, laoreet eget fermentum quis, ' +
+        'adipiscing et purus. Suspendisse nec condimentum leo. Ut ultrices malesuada ' +
+        'eros, vel imperdiet nunc elementum vel. Donec vitae orci arcu. ' +
+        'Etiam at turpis a ipsum pellentesque placerat. </p><br/><br/>'
+        
+        + '</div>'
+});

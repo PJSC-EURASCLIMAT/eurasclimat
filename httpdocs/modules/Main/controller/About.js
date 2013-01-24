a1a830974b2b655239adf043db77b4a9e6336e69
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-01-24 21:10:05 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@381 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/Main/controller/About.js
+++ httpdocs/modules/Main/controller/About.js
@@ -7,5 +7,9 @@ Ext.define('EC.Main.controller.About', {
     init: function(container) {
         
         container.add(this.getView('EC.Main.view.About').create());
+        container.addTool({type: 'pin'});
+        container.addTool({type: 'unpin'});
+        container.addTool({type: 'plus'});
+        container.addTool({type: 'minus'});
     }
 });

7d99135296ee56b7b63a56d1bfcf67af51b76cfb
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-03-10 18:58:32 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@460 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Main/view/About/Layout.js
@@ -0,0 +1,23 @@
+Ext.define('EC.Main.view.About.Layout', {
+
+    extend: 'App.view.PortalLayoutAbstarct',
+
+    title: 'О системе',
+    
+    icon: '/images/icons/about.png',
+
+    items: [{
+        title: 'Панель виджетов',
+        xtype: 'portalpanel',
+        border: false,
+        closable: false,
+        columns: 3,
+        items: [{
+            id: 'MainPanel-column-1'
+        }, {
+            id: 'MainPanel-column-2'
+        }, {
+            id: 'MainPanel-column-3'
+        }]
+    }]
+});

fa139307f9097ba9140015fd7e86984873af54ac
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-03-10 19:02:01 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@467 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Main/view/NewChapter/Layout.js
@@ -0,0 +1,9 @@
+Ext.define('EC.Main.view.NewChapter.Layout', {
+
+    extend: 'App.view.PortalLayoutAbstarct',
+
+    title: 'Создать свой подраздел',
+    
+    icon: '/images/icons/about.png'
+
+});

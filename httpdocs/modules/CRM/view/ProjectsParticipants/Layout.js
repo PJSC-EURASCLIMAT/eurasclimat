973f9172c53321e506c30be710191af86fdd7948
anton <anton@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-03-13 07:31:47 +0000

    названия кнопок и разделов изменены
    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@490 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/CRM/view/ProjectsParticipants/Layout.js
+++ httpdocs/modules/CRM/view/ProjectsParticipants/Layout.js
@@ -2,7 +2,7 @@ Ext.define('EC.CRM.view.ProjectsParticipants.Layout', {
 
     extend: 'App.view.PortalLayoutAbstarct',
 
-    title: 'Личные инженерные проекты по развитию Портала',
+    title: 'Личные проекты по развитию Портала',
     
     icon: '/images/icons/about.png'
 });

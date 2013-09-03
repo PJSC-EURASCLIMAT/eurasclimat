71259c6f563c147b5d568d2b6b9d86da88d4022f
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-09-03 21:37:11 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@735 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate-2013-09-04.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+CCREATE TABLE `auth_keys` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+ALTER TABLE `auth_keys`
+  ADD CONSTRAINT `auth_keys_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+SET FOREIGN_KEY_CHECKS=1;

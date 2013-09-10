3eeab4d5a28f5e2842333eb73a9aa193e7651d64
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-09-10 21:41:10 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@788 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/Market/controller/NewProjects.js
+++ httpdocs/modules/Market/controller/NewProjects.js
@@ -6,112 +6,112 @@ Ext.define('EC.Market.controller.NewProjects', {
         'EC.Market.view.NewProjects.Layout'
     ],
     
+    modules: [{
+        title: 'Мегафон',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site16'
+    }, {
+        title: 'МГТС',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site13'
+    }, {
+        title: 'Газпромбанк',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site10'
+    }, {
+        title: 'Почта России',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site7'
+    }, {
+        title: 'Госзакупки',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site4'
+    }, {
+        title: 'ЭТП ММББ',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-1',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site1'
+    }, {
+        title: 'МТС',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-2',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site14'
+    }, {
+        title: 'Газпром',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-2',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site11'
+    }, {
+        title: 'ВТБ24',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-2',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site8'
+    }, {
+        title: 'Аэрофлот',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-2',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site5'
+    }, {
+        title: 'Сбербанк-АСТ',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-2',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site2'
+    }, {
+        title: 'Билайн',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-3',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site15'
+    }, {
+        title: 'МГУ',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-3',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site12'
+    }, {
+        title: 'Лукойл',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-3',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site9'
+    }, {
+        title: 'Закупки Сбербанк-АСТ',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-3',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site6'
+    }, {
+        title: 'ЕЭТП (Росэлторг)',
+        icon: '/images/icons/about.png',
+        position: 'Market-NewProjects-column-3',
+        allowMultiple: true,
+        launchModule: 'EC.Market.controller.NewProjects.Site3'
+    }],
+    
     run: function(container) {
         
         this.getContainer(container);
         
         var MC = this.getController('App.controller.Main');
-
-        var modulesToOpen = [{
-            title: 'Мегафон',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site16'
-        }, {
-            title: 'МГТС',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site13'
-        }, {
-            title: 'Газпромбанк',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site10'
-        }, {
-            title: 'Почта России',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site7'
-        }, {
-            title: 'Госзакупки',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site4'
-        }, {
-            title: 'ЭТП ММББ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site1'
-        }, {
-            title: 'МТС',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site14'
-        }, {
-            title: 'Газпром',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site11'
-        }, {
-            title: 'ВТБ24',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site8'
-        }, {
-            title: 'Аэрофлот',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site5'
-        }, {
-            title: 'Сбербанк-АСТ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site2'
-        }, {
-            title: 'Билайн',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site15'
-        }, {
-            title: 'МГУ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site12'
-        }, {
-            title: 'Лукойл',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site9'
-        }, {
-            title: 'Закупки Сбербанк-АСТ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site6'
-        }, {
-            title: 'ЕЭТП (Росэлторг)',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site3'
-        }];
         
         container.on('show', function() {
-            Ext.each(modulesToOpen, function(item) {
+            Ext.each(this.modules, function(item) {
                 MC.openModulePortlet(item);
             });
         }, this, {single: true});
@@ -122,102 +122,13 @@ Ext.define('EC.Market.controller.NewProjects', {
         
         var MC = this.getController('App.controller.Main');
         
-        return [{
-            title: 'Мегафон',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site16'
-        }, {
-            title: 'МГТС',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site13'
-        }, {
-            title: 'Газпромбанк',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site10'
-        }, {
-            title: 'Почта России',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site7'
-        }, {
-            title: 'Госзакупки',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site4'
-        }, {
-            title: 'ЭТП ММББ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-1',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site1'
-        }, {
-            title: 'МТС',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site14'
-        }, {
-            title: 'Газпром',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site11'
-        }, {
-            title: 'ВТБ24',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site8'
-        }, {
-            title: 'Аэрофлот',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site5'
-        }, {
-            title: 'Сбербанк-АСТ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-2',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site2'
-        }, {
-            title: 'Билайн',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site15'
-        }, {
-            title: 'МГУ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site12'
-        }, {
-            title: 'Лукойл',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site9'
-        }, {
-            title: 'Закупки Сбербанк-АСТ',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site6'
-        }, {
-            title: 'ЕЭТП (Росэлторг)',
-            icon: '/images/icons/about.png',
-            position: 'Market-NewProjects-column-3',
-            allowMultiple: true,
-            launchModule: 'EC.Market.controller.NewProjects.Site3'
-        }];
+        Ext.each(this.modules, function(item) {
+            item.text = item.title;
+            item.handler = function(b) {
+                MC.openModulePortlet(b.initialConfig);
+            }
+        });
+        
+        return this.modules;
     }
 });

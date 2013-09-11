6b999eda30881c93f28b1fa7a1cfd177e0677c04
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-09-11 21:21:43 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@793 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate-2013-09-12.sql
+++ db/migrate-2013-09-12.sql
@@ -2,15 +2,17 @@ SET FOREIGN_KEY_CHECKS=0;
 
 CREATE TABLE `crm_orders` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` int(10) DEFAULT NULL,
+  `account_id` int(11) unsigned NOT NULL,
   `category` int(10) DEFAULT NULL,
   `object` int(10) DEFAULT NULL,
   `area` int(10) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `skype` varchar(255) DEFAULT NULL,
+  `contacts` varchar(255) DEFAULT NULL,
   `info` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
+ALTER TABLE `crm_orders`
+  ADD CONSTRAINT `crm_orders_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 SET FOREIGN_KEY_CHECKS=1;

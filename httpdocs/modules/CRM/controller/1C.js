b4aaf163d05be052d1494a98fef231d95e87260d
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-04 23:21:27 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@841 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/CRM/controller/1C.js
+++ httpdocs/modules/CRM/controller/1C.js
@@ -2,7 +2,7 @@ Ext.define('EC.CRM.controller.1C', {
 
     extend: 'Ext.app.Controller',
 
-    views: ['EC.CRM.view.CurrentProjects.1C'],
+    views: ['EC.CRM.view.1C'],
     
     run: function(container, module) {
         container.add(this.getView(this.views[0]).create());

14e6b091b1d3dd10f5ba480aa1f952afb3a6ebb4
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-05 03:33:15 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@849 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Market/controller/SiteView.js
@@ -0,0 +1,51 @@
+Ext.define('EC.Market.controller.SiteView', {
+    
+    extend: 'App.controller.PortalAbstract',
+
+    views: [
+        'EC.Market.view.Trade.SiteView',
+        'EC.Market.view.Trade.List'
+    ],
+
+    refs: [
+        {
+            ref: 'list',
+            selector: 'TradeViewList'
+        },{
+            ref: 'frame',
+            selector: 'TradeView #tradeFrame'
+        }
+    ],
+
+
+    run:function(container) {
+
+        this.control({
+            'TradeViewList': {
+                rowClicked: this.openSite
+            },
+            'TradeView' : {
+               afterlayout: this.viewAfterLayout
+            }
+        });
+
+        container.add(this.getView(this.views[0]).create());
+
+    }
+
+    ,openSite: function(url){
+        this.getFrame().el.down('iframe').set({src: "" + url + ""});
+    }
+
+    ,viewAfterLayout: function ( view, layout, eOpts ) {
+        document.getElementById('eventsIFrame').contentWindow.document.getElementsByTagName('body')[0].innerHTML = "Выберите сайт из списка слева";
+//        this.selectFirstRow();
+    }
+
+    ,selectFirstRow: function(){
+        this.getList().getView().select(0);
+        var url = this.getList().store.getAt(0).get('url');
+        this.openSite(url);
+    }
+
+});

dbf30e03f7ed34c106908cdf7d3e708115ba1d63
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-05 03:38:09 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@855 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/app/controller/Interface/GoodsServices.js
+++ httpdocs/app/controller/Interface/GoodsServices.js
@@ -11,32 +11,9 @@ Ext.define('App.controller.Interface.GoodsServices', {
         var container = this.getContainer();
         
         this.getController('EC.Catalog.controller.GoodsCatalog').run(container);
-        /*
-        this.getController('EC.Catalog.controller.VendorCatalog').run(container);
-        this.getController('EC.Catalog.controller.NewChapter').run(container);
-        */
     },
     
     getMenu: function() {
-        return [
-        /*
-            {
-            text: 'Функции',
-            icon: '/images/icons/features.png'
-        }, {
-            text: 'Сервисы',
-            icon: '/images/icons/services.png'
-        }, {
-            text: 'Документы',
-            icon: '/images/icons/documents.png'
-        }, {
-            text: 'Конструктор',
-            icon: '/images/icons/constructor.png'
-        }, {
-            text: 'Настройки',
-            icon: '/images/icons/settings.png'
-        }
-        */
-        ];
+        return [];
     }
 });

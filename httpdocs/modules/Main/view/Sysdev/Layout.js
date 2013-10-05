ca503e13fd3710ea4ec1b0f7398301197681a341
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-05 13:57:02 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@877 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Main/view/Sysdev/Layout.js
@@ -0,0 +1,37 @@
+Ext.define('EC.Main.view.Sysdev.Layout', {
+    
+    extend: 'Ext.panel.Panel',
+    
+    alias: 'widget.SysdevLayout',
+    
+    layout: 'border',
+    
+    border: false,
+    
+    items: [{
+        xtype: 'SysdevThemesTree',
+        region: 'west',
+        width: 170
+    }, {
+        xtype: 'panel',
+        type: 'preview',
+        bodyPadding: 5,
+        autoScroll: true,
+        loader: {
+            url: '/sysdev/description/get-description-content',
+            loadMask: true
+        },
+        layout: 'fit',
+        region: 'center',
+        dockedItems: [{
+            xtype: 'toolbar',
+            dock: 'bottom', 
+            hidden: !acl.isUpdate('sysdev'),
+            items: [{
+                text: 'Редактировать',
+                action: 'edit'
+            }]
+        }]
+    }]
+    
+});

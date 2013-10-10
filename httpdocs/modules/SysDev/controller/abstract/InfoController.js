35a93a2b28e01ef97b73076af65261754cae1b0f
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-10 17:21:02 +0000

    j
    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@906 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/SysDev/controller/abstract/InfoController.js
@@ -0,0 +1,69 @@
+Ext.define('EC.SysDev.controller.abstract.InfoController', {
+    
+    extend: 'Ext.app.Controller',
+    
+    currentProjectId: null,
+
+    onProjectSelected: function(record) {
+        console.log("abstract info controller PROJECT SELECTED");
+        
+//        if (record.get('stage') !== this.projectStageCode) {
+//            return;
+//        }
+
+        if (record.get('leaf') != true) {
+            return;
+        }
+//
+        if (!acl.isView('projectdev', 'info')) {
+            return;
+        }
+        
+        this.currentProjectId = record.get('id');
+
+        this.getInfo().getLoader().load({
+            params: {
+                id: this.currentProjectId
+            }
+        });
+
+    },
+            
+    onEditButtonClick: function() {
+
+        var infoPanel = this.getInfo();
+        infoPanel.hide();
+
+        this.fireEvent(
+            'project-description-editing-requested', 
+            this.projectStageCode,
+            this.currentProjectId
+        );
+
+    },
+            
+    onEditorHidden: function(projectStageCode) {
+
+        if (projectStageCode !== this.projectStageCode) {
+            return;
+        }
+        
+        var infoPanel = this.getInfo();
+        
+        infoPanel.show();
+
+    },
+            
+    onProjectInfoUpdated: function(record) {
+
+        var infoPanel = this.getInfo();
+        
+        this.getInfo().getLoader().load({
+            params: {
+                id: record.get('id')
+            }
+        });
+
+    }
+    
+});

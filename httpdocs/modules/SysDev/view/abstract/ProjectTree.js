c5fa0cf37ea20a962387793b3ee5bc590d71e2f3
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-18 00:28:55 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@956 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/SysDev/view/abstract/ProjectTree.js
+++ httpdocs/modules/SysDev/view/abstract/ProjectTree.js
@@ -17,60 +17,83 @@ Ext.define('EC.SysDev.view.abstract.ProjectTree', {
     useArrows: true,
     
     scroll: 'vertical',
-
-    viewConfig: {
-        plugins: [{
-            ptype: 'treeviewdragdrop', 
-            pluginId: 'project-tree-drag-and-drop-plugin', 
-            containerScroll: true
-        }]
-    },
     
-    plugins: [{
-        ptype: 'cellediting', 
-        pluginId: 'project-tree-cell-editing-plugin'
-    }],
+    permissions: acl.isUpdate('sysdev', 'info'),
 
-    selModel: {
-        selection: 'treemodel',
-        mode: 'MULTI'
-    },
+    initComponent: function() {
     
-    columns: [
-        {
+        if (this.permissions) {
+            
+            this.viewConfig = {
+                plugins: [{
+                    ptype: 'treeviewdragdrop', 
+                    pluginId: 'project-tree-drag-and-drop-plugin', 
+                    containerScroll: true
+                }]
+            };
+        
+            this.plugins = [{
+                ptype: 'cellediting', 
+                pluginId: 'project-tree-cell-editing-plugin'
+            }];
+        
+            this.selModel = {
+                selection: 'treemodel',
+                mode: 'MULTI'
+            };
+        }
+        
+        var columnsList = [];
+        
+        var treeColumn = {
             xtype: 'treecolumn',
             dataIndex: 'name',
             flex: 1,
             renderer: function(value, metadata) {
                 metadata.tdAttr = 'data-qtip="' + value + '"';
                 return value;
-            },
-            listeners: {
-                itemdblclick: function( column, record, item, index, e, eOpts ) {
-//                    console.log("itemdblclick");
-                }
             }
-            ,editor: {
+        };
+        
+        if (this.permissions) {
+            
+            treeColumn.listeners = {
+                itemdblclick: function( column, record, item, index, e, eOpts ) {
+                    console.log("itemdblclick");
+                }
+            };
+            
+            treeColumn.editor = {
                 xtype: 'textfield',
                 allowBlank: false
-            }
-        }, {
-            xtype:'actioncolumn',
-            hidden: !acl.isUpdate('sysdev', 'info'),
-            width: 20,
-            items: [{     
-                text: 'Переименовать',
-                icon: '/images/icons/fam/cog.png',
-                handler: function(treeView, rowIndex, u, button, event, record, rowEl) {
-                    var tree = treeView.ownerCt;
-                    var selectionModel = tree.getSelectionModel();
-                    if (!selectionModel.isSelected(record)) {
-                        selectionModel.select(record);
-                    }
-                    tree.fireEvent('project-tree-context-menu-requested', record, event.getXY());
-                }
-            }]
+            };
         }
-    ]
-    
+        
+        columnsList.push(treeColumn);
+        
+        if (this.permissions) {
+        
+            columnsList.push({
+                xtype:'actioncolumn',
+                hidden: !acl.isUpdate('sysdev', 'info'),
+                width: 20,
+                items: [{     
+                    text: 'Переименовать',
+                    icon: '/images/icons/fam/cog.png',
+                    handler: function(treeView, rowIndex, u, button, event, record, rowEl) {
+                        var tree = treeView.ownerCt;
+                        var selectionModel = tree.getSelectionModel();
+                        if (!selectionModel.isSelected(record)) {
+                            selectionModel.select(record);
+                        }
+                        tree.fireEvent('project-tree-context-menu-requested', record, event.getXY());
+                    }
+                }]
+            });
+        }
+        
+        this.columns = columnsList;
+        
+        this.callParent();
+    }
 });

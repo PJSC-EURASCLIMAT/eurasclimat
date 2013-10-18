7732a6efbcbef8bcb85742fae131f83c19a1c565
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-18 00:50:00 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@958 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/Orders/view/Layout.js
+++ httpdocs/modules/Orders/view/Layout.js
@@ -17,36 +17,36 @@ Ext.define('EC.Orders.view.Layout', {
             items: [{
                 title: 'Предварительные согласования',
                 layout: 'fit',
-                items: [this.getPanel()]
+                items: [this.getPanel('"Формирование проектов", <br/>  стадии "Предварительные согласования"')]
             }, {
                 title: 'Подготовка договора',
                 layout: 'fit',
-                items: [this.getPanel()]
+                items: [this.getPanel('"Формирование проектов", <br/>  стадии "Подготовка договора"')]
             }]
         }, {
             title: 'Исполнение проектов',
             items: [{
                 title: 'Выполнение договора',
                 layout: 'fit',
-                items: [this.getPanel()]
+                items: [this.getPanel('"Исполнение проектов", <br/>  стадии "Выполнение договора"')]
             }, {
                 title: 'Внедрение',
                 layout: 'fit',
-                items: [this.getPanel()]
+                items: [this.getPanel('"Исполнение проектов", <br/>  стадии "Внедрение"')]
             }]
         }];
         
         this.callParent();
     },
     
-    getPanel: function() {
+    getPanel: function(titleText) {
         return Ext.create('Ext.panel.Panel', {
             layout: 'border',
             items: [{
                 region: 'west',
                 width: 300,
                 bodyPadding: 10, 
-                html: 'Здесь список проектов' 
+                html: 'Cписок проектов <br/> раздела ' + titleText 
             }, {
                 region: 'center',
                 bodyPadding: 10,

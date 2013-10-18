2e443c9b0787923f876db91ffb17f1818d4930a1
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-18 00:01:54 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@953 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,2 +1,2 @@
-DELETE FROM `e-head_ec`.`acl_resources` WHERE `acl_resources`.`name` = 'sysdev';
-UPDATE  `e-head_ec`.`acl_resources` SET  `name` =  'sysdev' WHERE  `acl_resources`.`name` = 'projectdev';
+DELETE FROM `acl_resources` WHERE `acl_resources`.`name` = 'sysdev';
+UPDATE  `acl_resources` SET  `name` =  'sysdev' WHERE  `acl_resources`.`name` = 'projectdev';

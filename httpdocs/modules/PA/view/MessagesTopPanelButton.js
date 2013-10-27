0c0c99a1500cd79948c07533c768a9be22e3d0cf
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-27 21:36:53 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@969 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/PA/view/MessagesTopPanelButton.js
@@ -0,0 +1,43 @@
+Ext.define('EC.PA.view.MessagesTopPanelButton', {
+    
+    extend: 'Ext.container.Container',
+
+    alias: 'widget.top-panel-msg-button',
+
+    padding: '8 0 8 0',
+
+    items: [
+        {
+            xtype: 'button',
+            tooltip: 'Сообщения',
+            bodyStyle: 'background:transparent;',
+            icon: '/images/icons/messages.png',
+            arrowCls: '',
+            frame: false,
+            border: false,
+            style: {
+                background: 'transparent !important'
+            },
+            hidden: !isAuth,
+            action: 'messages',
+            launchModule: 'EC.PA.controller.Messages'
+        }
+        ,{
+            xtype: 'box',
+            hidden: true,
+            itemId: 'count',
+            cls: 'top-panel-messages-number',
+            html: ''
+        }
+    ]
+    
+    ,updateCount: function(num) {
+        var counter = this.down("#count");
+        counter.update(num);
+        if(num === 0 && !counter.isHidden()) {
+            counter.hide();
+        } else if(num > 0 && counter.isHidden()) {
+            counter.show();
+        }
+    }
+});

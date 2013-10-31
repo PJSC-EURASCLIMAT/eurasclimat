59415321002017346f93df27c8b20141cc565000
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-10-31 20:12:52 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@993 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate-2013-10-25.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE `messages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sender_id` int(10) unsigned DEFAULT NULL,
+  `receiver_id` int(10) unsigned DEFAULT NULL,
+  `message` text,
+  `date` timestamp NULL DEFAULT NULL,
+  `readed` tinyint(4) DEFAULT '0',
+  `parent` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sender_id` (`sender_id`),
+  KEY `receiver_id` (`receiver_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
+
+SET FOREIGN_KEY_CHECKS=1;

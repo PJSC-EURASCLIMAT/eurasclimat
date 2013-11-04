a0bf594bc5adb6a10d72bdf8c157c9b0546ba1f9
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-11-04 11:20:37 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1019 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/SysDev/view/InfoEditor.js
+++ httpdocs/modules/SysDev/view/InfoEditor.js
@@ -9,24 +9,21 @@ Ext.define('EC.SysDev.view.InfoEditor', {
     trackResetOnLoad: true, // позволяет следить за изменениями в полях формы через событие dirtychange
 
     border: false,
+    
     autoScroll: true,
 
-    tbar: [
-        {
-            xtype: 'button',
-            text: 'Детальное описание',
-            itemId: 'full_desc-button'
-        },
-        '->',
-        {
-            text: 'Отменить',
-            itemId: 'cancel-button'
-        }, {
-            text: 'Сохранить',
-            itemId: 'save-button',
-            disabled: true
-        }
-    ],
+    tbar: [{
+        xtype: 'button',
+        text: 'Детальное описание',
+        itemId: 'full_desc-button'
+    }, '->', {
+        text: 'Отменить',
+        itemId: 'cancel-button'
+    }, {
+        text: 'Сохранить',
+        itemId: 'save-button',
+        disabled: true
+    }],
 
     defaults: {
         labelWidth: 150,
@@ -40,65 +37,60 @@ Ext.define('EC.SysDev.view.InfoEditor', {
         }
     },
 
-    items: [
-        {
-            xtype: 'hiddenfield',
-            name: 'id'
-        },
-        {
-            fieldLabel: 'Наименование проекта',
-            xtype: 'textfield',
-            allowBlank: false,
-            name: 'name'
-        }, {
-            fieldLabel: 'Описание проекта',
-            xtype: 'htmleditor',
-            name: 'description',
-            maxWidth: null
-        }, {
-            fieldLabel: 'Инициатор проекта',
-            xtype: 'combobox',
-            name: 'account_id',
-            displayField: 'name',
-            valueField: 'id',
-            queryMode: 'local',
-            store: {
-                type: 'project-account-store',
-                autoLoad: true,
-                sorters: [{property:'name', direction: 'ASC'}],
-                sortOnLoad: true
-            }
-        }, {
-            fieldLabel: 'Бюджет',
-            xtype: 'numberfield',
-            name: 'budget',
-            minValue: 0,
-            step: 1000
-        }, {
-            fieldLabel: 'Планируемые сроки проекта',
-            xtype: 'fieldcontainer',
-            layout: 'hbox',
-            items: [
-                {
-                    xtype: 'datefield',
-                    name: 'date_plan_begin',
-                    format: 'Y-m-d H:i:s',
-                    flex: 1
-                }, {
-                    xtype: 'splitter'
-                }, {
-                    xtype: 'datefield',
-                    name: 'date_plan_end',
-                    format: 'Y-m-d H:i:s',
-                    flex: 1
-                }
-            ]
-        }, {
-            fieldLabel: 'Дата фактического выполнения',
-            xtype: 'datefield',
-            name: 'date_fact_end',
-            format: 'Y-m-d H:i:s'
+    items: [{
+        xtype: 'hiddenfield',
+        name: 'id'
+    }, {
+        fieldLabel: 'Наименование проекта',
+        xtype: 'textfield',
+        allowBlank: false,
+        name: 'name'
+    }, {
+        fieldLabel: 'Описание проекта',
+        xtype: 'htmleditor',
+        name: 'description',
+        flex: 1
+    }, {
+        fieldLabel: 'Инициатор проекта',
+        xtype: 'combobox',
+        name: 'account_id',
+        displayField: 'name',
+        valueField: 'id',
+        queryMode: 'local',
+        store: {
+            type: 'project-account-store',
+            autoLoad: true,
+            sorters: [{property:'name', direction: 'ASC'}],
+            sortOnLoad: true
         }
-    ]
+    }, {
+        fieldLabel: 'Бюджет',
+        xtype: 'numberfield',
+        name: 'budget',
+        minValue: 0,
+        step: 1000
+    }, {
+        fieldLabel: 'Планируемые сроки проекта',
+        xtype: 'fieldcontainer',
+        layout: 'hbox',
+        items: [{
+            xtype: 'datefield',
+            name: 'date_plan_begin',
+            format: 'Y-m-d H:i:s',
+            flex: 1
+        }, {
+            xtype: 'splitter'
+        }, {
+            xtype: 'datefield',
+            name: 'date_plan_end',
+            format: 'Y-m-d H:i:s',
+            flex: 1
+        }]
+    }, {
+        fieldLabel: 'Дата фактического выполнения',
+        xtype: 'datefield',
+        name: 'date_fact_end',
+        format: 'Y-m-d H:i:s'
+    }]
     
 });

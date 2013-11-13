8a309e81ced0b9987b2e45a33539d868fd05780d
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-11-13 19:20:38 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1068 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,48 +1,32 @@
---
--- Структура таблицы `files`
---
+SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS `files`;
 CREATE TABLE IF NOT EXISTS `files` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(255) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `account_id` int(10) unsigned DEFAULT NULL,
+  `account_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `account_id` (`account_id`),
-  KEY `account_id_2` (`account_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+  KEY `account_id` (`account_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
---
--- Ограничения внешнего ключа таблицы `files`
---
 ALTER TABLE `files`
   ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
-
-
---
--- Структура таблицы `main_sysdev_project_docs`
---
 
 DROP TABLE IF EXISTS `main_sysdev_project_docs`;
 CREATE TABLE IF NOT EXISTS `main_sysdev_project_docs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` int(11) unsigned DEFAULT NULL,
-  `project_id` int(11) unsigned DEFAULT NULL,
-  `file_id` int(11) DEFAULT NULL,
+  `project_id` int(11) unsigned NOT NULL,
+  `file_id` int(11) unsigned NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `main_sysdev_project_docs_ibfk_1` (`account_id`),
-  KEY `main_sysdev_project_docs_ibfk_2` (`project_id`),
-  KEY `file_id` (`file_id`),
-  KEY `file_id_2` (`file_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=87 ;
+  KEY `project_id` (`project_id`),
+  KEY `file_id` (`file_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Ограничения внешнего ключа таблицы `main_sysdev_project_docs`
---
 ALTER TABLE `main_sysdev_project_docs`
-  ADD CONSTRAINT `main_sysdev_project_docs_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `main_sysdev_project_docs_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `main_sysdev_projects` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `main_sysdev_project_docs_ibfk_4` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `main_sysdev_project_docs_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `main_sysdev_projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `main_sysdev_project_docs_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE;
+
+ SET FOREIGN_KEY_CHECKS=1;

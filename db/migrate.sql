064a491d789661b1d74411949dfbbc227777d035
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-11-13 19:39:19 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1069 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -11,9 +11,6 @@ CREATE TABLE IF NOT EXISTS `files` (
   KEY `account_id` (`account_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-ALTER TABLE `files`
-  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
-
 DROP TABLE IF EXISTS `main_sysdev_project_docs`;
 CREATE TABLE IF NOT EXISTS `main_sysdev_project_docs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -25,6 +22,9 @@ CREATE TABLE IF NOT EXISTS `main_sysdev_project_docs` (
   KEY `file_id` (`file_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+ALTER TABLE `files`
+  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
+  
 ALTER TABLE `main_sysdev_project_docs`
   ADD CONSTRAINT `main_sysdev_project_docs_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `main_sysdev_projects` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `main_sysdev_project_docs_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE;

065f442337011eb72f634c2ac55a2725d10212f1
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-12-08 20:15:55 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1171 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,8 +1,9 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE `experts` (
+DROP TABLE IF EXISTS `experts`;
+CREATE TABLE IF NOT EXISTS `experts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `account_id` int(10) unsigned DEFAULT NULL,
   `desc` text,
   `city_id` int(10) unsigned DEFAULT NULL,
   `status_id` int(10) unsigned DEFAULT NULL,
@@ -14,11 +15,30 @@ CREATE TABLE `experts` (
   KEY `status_id` (`status_id`) USING BTREE,
   KEY `equip_id` (`equip_id`) USING BTREE,
   KEY `author_id` (`author_id`) USING BTREE,
-  KEY `city_id` (`city_id`) USING BTREE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  KEY `city_id` (`city_id`) USING BTREE,
+  KEY `account_id` (`account_id`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 
-CREATE TABLE `experts_docs` (
+DROP TRIGGER IF EXISTS `experts_date_create`;
+DELIMITER //
+CREATE TRIGGER `experts_date_create` BEFORE INSERT ON `experts`
+ FOR EACH ROW BEGIN
+set NEW.date_create = NOW();
+set NEW.date_update = NOW();
+END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `experts_date_udpate`;
+DELIMITER //
+CREATE TRIGGER `experts_date_udpate` BEFORE UPDATE ON `experts`
+ FOR EACH ROW SET NEW.date_update = NOW()
+//
+DELIMITER ;
+
+
+DROP TABLE IF EXISTS `experts_docs`;
+CREATE TABLE IF NOT EXISTS `experts_docs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `desc` text,
@@ -31,10 +51,27 @@ CREATE TABLE `experts_docs` (
   KEY `doc_type_id` (`doc_type_id`),
   KEY `expert_id` (`expert_id`),
   KEY `author_id` (`author_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TRIGGER IF EXISTS `ex_docs_date_create`;
+DELIMITER //
+CREATE TRIGGER `ex_docs_date_create` BEFORE INSERT ON `experts_docs`
+ FOR EACH ROW BEGIN
+set NEW.date_create = NOW();
+set NEW.date_update = NOW();
+END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `ex_docs_date_update`;
+DELIMITER //
+CREATE TRIGGER `ex_docs_date_update` BEFORE UPDATE ON `experts_docs`
+ FOR EACH ROW SET NEW.date_update = NOW()
+//
+DELIMITER ;
 
 
-CREATE TABLE `experts_equipment` (
+DROP TABLE IF EXISTS `experts_equipment`;
+CREATE TABLE IF NOT EXISTS `experts_equipment` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `date_create` datetime DEFAULT NULL,
@@ -42,10 +79,27 @@ CREATE TABLE `experts_equipment` (
   `author_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `author_id` (`author_id`) USING BTREE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 
-CREATE TABLE `experts_experience` (
+DROP TRIGGER IF EXISTS `ex_equipment_date_create`;
+DELIMITER //
+CREATE TRIGGER `ex_equipment_date_create` BEFORE INSERT ON `experts_equipment`
+ FOR EACH ROW BEGIN
+set NEW.date_create = NOW();
+set NEW.date_update = NOW();
+END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `ex_equipment_date_update`;
+DELIMITER //
+CREATE TRIGGER `ex_equipment_date_update` BEFORE UPDATE ON `experts_equipment`
+ FOR EACH ROW SET NEW.date_update = NOW()
+//
+DELIMITER ;
+
+DROP TABLE IF EXISTS `experts_experience`;
+CREATE TABLE IF NOT EXISTS `experts_experience` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `desc` text,
@@ -57,10 +111,12 @@ CREATE TABLE `experts_experience` (
   PRIMARY KEY (`id`),
   KEY `expert_id` (`expert_id`),
   KEY `doc_id` (`doc_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
-CREATE TABLE `experts_job_types` (
+
+DROP TABLE IF EXISTS `experts_job_types`;
+CREATE TABLE IF NOT EXISTS `experts_job_types` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `date_create` datetime DEFAULT NULL,
@@ -69,10 +125,28 @@ CREATE TABLE `experts_job_types` (
   `parent_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `author_id` (`author_id`) USING BTREE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 
-CREATE TABLE `experts_rating` (
+DROP TRIGGER IF EXISTS `ex_job_types_date_create`;
+DELIMITER //
+CREATE TRIGGER `ex_job_types_date_create` BEFORE INSERT ON `experts_job_types`
+ FOR EACH ROW BEGIN
+set NEW.date_create = NOW();
+set NEW.date_update = NOW();
+END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `ex_job_types_date_update`;
+DELIMITER //
+CREATE TRIGGER `ex_job_types_date_update` BEFORE UPDATE ON `experts_job_types`
+ FOR EACH ROW SET NEW.date_update = NOW()
+//
+DELIMITER ;
+
+
+DROP TABLE IF EXISTS `experts_rating`;
+CREATE TABLE IF NOT EXISTS `experts_rating` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `date_create` datetime DEFAULT NULL,
@@ -83,6 +157,21 @@ CREATE TABLE `experts_rating` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 
+DROP TRIGGER IF EXISTS `ex_rating_date_create`;
+DELIMITER //
+CREATE TRIGGER `ex_rating_date_create` BEFORE INSERT ON `experts_rating`
+ FOR EACH ROW BEGIN
+set NEW.date_create = NOW();
+set NEW.date_update = NOW();
+END
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `ex_rating_date_update`;
+DELIMITER //
+CREATE TRIGGER `ex_rating_date_update` BEFORE UPDATE ON `experts_rating`
+ FOR EACH ROW SET NEW.date_update = NOW()
+//
+DELIMITER ;
 
 DROP TABLE IF EXISTS `experts_statuses`;
 CREATE TABLE IF NOT EXISTS `experts_statuses` (
@@ -94,31 +183,32 @@ CREATE TABLE IF NOT EXISTS `experts_statuses` (
   `author_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `author_id` (`author_id`) USING BTREE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 
-DROP TRIGGER IF EXISTS `date_create`;
+
+DROP TRIGGER IF EXISTS `ex_statuses_date_create`;
 DELIMITER //
-CREATE TRIGGER `date_create` BEFORE INSERT ON `experts_statuses`
+CREATE TRIGGER `ex_statuses_date_create` BEFORE INSERT ON `experts_statuses`
  FOR EACH ROW BEGIN
 set NEW.date_create = NOW();
 set NEW.date_update = NOW();
 END
 //
 DELIMITER ;
-DROP TRIGGER IF EXISTS `date_update`;
+DROP TRIGGER IF EXISTS `ex_statuses_date_update`;
 DELIMITER //
-CREATE TRIGGER `date_update` BEFORE UPDATE ON `experts_statuses`
+CREATE TRIGGER `ex_statuses_date_update` BEFORE UPDATE ON `experts_statuses`
  FOR EACH ROW SET NEW.date_update = NOW()
 //
 DELIMITER ;
 
-
 ALTER TABLE `experts`
-  ADD CONSTRAINT `experts_ibfk_4` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `experts_ibfk_1` FOREIGN KEY (`equip_id`) REFERENCES `experts_equipment` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `experts_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `experts_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `experts_statuses` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `experts_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `experts_statuses` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `experts_ibfk_4` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `experts_ibfk_5` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
 
 ALTER TABLE `experts_docs`
   ADD CONSTRAINT `experts_docs_ibfk1` FOREIGN KEY (`doc_type_id`) REFERENCES `doc_types` (`id`) ON DELETE SET NULL,
@@ -140,8 +230,5 @@ ALTER TABLE `experts_rating`
 
 ALTER TABLE `experts_statuses`
   ADD CONSTRAINT `experts_statuses_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL;
-
-
-
 
 SET FOREIGN_KEY_CHECKS=1;

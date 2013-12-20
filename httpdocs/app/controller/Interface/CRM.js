8b3b2e646519ffed817902dce55b13f249a72af9
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2013-12-20 21:54:30 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1205 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/app/controller/Interface/CRM.js
+++ httpdocs/app/controller/Interface/CRM.js
@@ -10,7 +10,6 @@ Ext.define('App.controller.Interface.CRM', {
         
         var container = this.getContainer();
         
-//        this.getController('App.controller.Interface.CRM.CurrentProjects').run(container);
         this.getController('App.controller.Interface.CRM.Development').run(container);
         this.getController('App.controller.Interface.CRM.Orders').run(container);
         
--- httpdocs/app/view/Interface/CRM/CurrentProjects.js
+++
@@ -1,8 +0,0 @@
-Ext.define('App.view.Interface.CRM.CurrentProjects', {
-
-    extend: 'App.view.PortalLayoutAbstarct',
-
-    title: 'Личные производственные проекты',
-    
-    icon: '/images/icons/manufac_projects.png'
-});

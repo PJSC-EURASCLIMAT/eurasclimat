268b68bcce5ba5c055af19b367e95ba4504d000d
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-01-15 19:53:16 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1240 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -20,17 +20,13 @@ ALTER TABLE `crm_projects_equipment_services`
   ADD CONSTRAINT `crm_projects_equipment_services_ibfk_1` 
     FOREIGN KEY (`service_id`) REFERENCES `catalog_services` (`id`);
 
+ALTER TABLE `crm_projects_equipment` ADD `price` TEXT NOT NULL;
 
-ALTER TABLE  `experts`
-  ADD  `experience` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL
-ALTER TABLE  `experts`
-  ADD  `rating` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL
+ALTER TABLE  `experts`  ADD  `experience` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE  `experts`  ADD  `rating` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 
-ALTER TABLE  `experts_docs`
-  ADD  `file_id` INT( 10 ) UNSIGNED NULL
-ALTER TABLE  `experts_docs`
-  ADD INDEX (  `file_id` )
-ALTER TABLE  `experts_docs`
-  ADD FOREIGN KEY (  `file_id` ) REFERENCES  `e-head_ec`.`files` (`id`)
-  ON DELETE SET NULL ON UPDATE RESTRICT ;
+ALTER TABLE  `experts_docs`  ADD  `file_id` INT( 10 ) UNSIGNED NULL;
+ALTER TABLE  `experts_docs`  ADD INDEX (  `file_id` );
+ALTER TABLE  `experts_docs`  ADD FOREIGN KEY (  `file_id` ) REFERENCES  `e-head_ec`.`files` (`id`)  ON DELETE SET NULL ON UPDATE RESTRICT ;
+
 SET FOREIGN_KEY_CHECKS=1;

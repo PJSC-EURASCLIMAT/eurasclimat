dadc1cbbe9536405f6de55f2c9e78e928bb139a4
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-02-02 20:23:45 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1251 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate-2014-02-02.sql
@@ -0,0 +1,17 @@
+SET FOREIGN_KEY_CHECKS=0;
+ALTER TABLE `experts` CHANGE `rating` `rating` INT(10) UNSIGNED NULL DEFAULT NULL;
+
+CREATE TABLE `experts2job_types` (
+  `expert_id` int(10) unsigned NOT NULL,
+  `job_type_id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `expert_id` (`expert_id`),
+  KEY `job_type_id` (`job_type_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+ALTER TABLE `experts2job_types`
+  ADD CONSTRAINT `experts2job_types_ibfk_1` FOREIGN KEY (`expert_id`) REFERENCES `experts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `experts2job_types_ibfk_3` FOREIGN KEY (`job_type_id`) REFERENCES `experts_job_types` (`id`) ON DELETE CASCADE;
+
+SET FOREIGN_KEY_CHECKS=1;

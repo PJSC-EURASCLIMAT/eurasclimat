f735ea4d09fe2a6c937e717455caf3f74764978f
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-02-05 02:41:18 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1255 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate-2014-02-04.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+ALTER TABLE `experts` 
+    ADD `work_years` INT( 2 ) NULL ,
+    ADD  `study_years` INT( 2 ) NULL ,
+    ADD  `sert_count` INT( 2 ) NULL ;
+ALTER TABLE `experts` DROP `experience` ;
+
+SET FOREIGN_KEY_CHECKS=1;
--- db/migrate.sql
+++ db/migrate.sql
@@ -1,7 +1,22 @@
 SET FOREIGN_KEY_CHECKS=0;
-ALTER TABLE  `experts` ADD  `work_years` INT( 2 ) NULL ,
-ADD  `study_years` INT( 2 ) NULL ,
-ADD  `sert_count` INT( 2 ) NULL ;
-ALTER TABLE  `experts` DROP  `experience` ;
+
+ALTER TABLE `crm_projects` 
+    ADD `customer_id` INT UNSIGNED NULL AFTER `creator_id`, 
+    ADD INDEX (`customer_id`) ;
+ALTER TABLE `crm_projects` 
+    ADD `manager_id` INT UNSIGNED NULL AFTER `customer_id`, 
+    ADD INDEX (`manager_id`) ;
+ALTER TABLE `crm_projects` ADD `stage` 
+    SET('preparation','coordination','execution','implementation') 
+    NOT NULL DEFAULT 'preparation' AFTER `manager_id`;
+ALTER TABLE `crm_projects` ADD `address` TEXT NULL AFTER `customer_id`;
+ALTER TABLE `crm_projects` ADD `description` TEXT NULL AFTER `address`;
+
+-- foreign keys --
+     
+ALTER TABLE `crm_projects` ADD FOREIGN KEY (`customer_id`) 
+    REFERENCES `accounts`(`id`) ON DELETE SET NULL ON UPDATE RESTRICT;
+ALTER TABLE `crm_projects` ADD FOREIGN KEY (`manager_id`) 
+    REFERENCES `accounts`(`id`) ON DELETE SET NULL ON UPDATE RESTRICT;
 
 SET FOREIGN_KEY_CHECKS=1;

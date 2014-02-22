1005f9f88bed7044e7db1976ed26d5d336a38947
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-02-22 18:39:50 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1295 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate-2014-02-22.sql
+++ db/migrate-2014-02-22.sql
@@ -1,5 +1,21 @@
 SET FOREIGN_KEY_CHECKS=0;
 
+ALTER TABLE `messages` ADD `type` INT(2) UNSIGNED NULL DEFAULT NULL ;
+ALTER TABLE `messages` ADD `subject` VARCHAR(255) NULL DEFAULT NULL ;
+ALTER TABLE `messages` ADD `deleted` INT(1) UNSIGNED NOT NULL DEFAULT '0';
+
+ALTER TABLE `messages` ADD `owner_id` INT(10) UNSIGNED NOT NULL, ADD INDEX (`owner_id`);
+
+ALTER TABLE `messages` 
+    ADD `sender_name` VARCHAR(255) NULL DEFAULT NULL,
+    ADD `receiver_name` VARCHAR(255) NULL DEFAULT NULL;
+
+ALTER TABLE `messages` ADD FOREIGN KEY (`receiver_id`) 
+    REFERENCES `accounts` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT;
+ALTER TABLE `messages` ADD FOREIGN KEY (`owner_id`) 
+    REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+
 DROP TABLE IF EXISTS `calcpd_obj_class`;
 CREATE TABLE IF NOT EXISTS `calcpd_obj_class` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,

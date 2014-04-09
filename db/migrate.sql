358c0e9d6c440eb5d97d7977143ec7cbb90f9d96
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-04-09 06:56:52 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1371 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,10 +1,3 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE  `courses` ADD  `closed` INT( 1 ) NOT NULL DEFAULT  '0';
-
-ALTER TABLE  `courses` DROP FOREIGN KEY  `courses_ibfk_1` ;
-ALTER TABLE  `courses` ADD CONSTRAINT  `courses_ibfk_1` FOREIGN KEY (  `type_id` ) REFERENCES  `e-head_ec`.`courses_groups` (
-`id`
-) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
 SET FOREIGN_KEY_CHECKS=1;

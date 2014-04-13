733240141a9597a3110ffef84a23deb693094532
sinyutin <sinyutin@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-04-13 08:06:14 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1377 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,6 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE  `courses` ADD  `offer_num` VARCHAR( 255 ) NULL ,
+ALTER TABLE  `courses` ADD  `offer_num` VARCHAR( 50 ) NULL ,
 ADD UNIQUE (`offer_num`);
 
 ALTER TABLE  `courses` ADD  `price` DOUBLE( 8, 2 ) NULL ;

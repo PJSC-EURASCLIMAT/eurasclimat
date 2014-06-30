f89d842485587be626d348191b3b592a19786ea7
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-06-30 09:58:34 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1424 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,16 +1,3 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE  `professions` DROP FOREIGN KEY  `professions_ibfk_2` ;
-ALTER TABLE  `professions` DROP INDEX `qualification_id` ;
-ALTER TABLE `professions` DROP `qualification_id`;
-
-ALTER TABLE  `professions` ADD  `qualification_type_id` INT( 10 ) UNSIGNED NULL ;
-ALTER TABLE  `professions` ADD INDEX (  `qualification_type_id` ) ;
-ALTER TABLE  `professions` ADD FOREIGN KEY (  `qualification_type_id` ) REFERENCES  `e-head_ec`.`qualifications_types` (
-`id`
-) ON DELETE SET NULL ON UPDATE RESTRICT ;
-
-ALTER TABLE `professions` DROP `factor`;
-
-
 SET FOREIGN_KEY_CHECKS=1;

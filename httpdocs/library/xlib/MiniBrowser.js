f82631f95454345cc5fee5aac2d42cec24f655e5
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-07-04 17:47:49 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1425 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/library/xlib/MiniBrowser.js
+++ httpdocs/library/xlib/MiniBrowser.js
@@ -4,7 +4,9 @@ Ext.define('xlib.MiniBrowser', {
 
     run: function(container) {
         
-        var iframe = Ext.create('Ext.Component', {
+        this.iframe = Ext.create('Ext.Component', {
+            itemId: 'minibrowseriframe',
+            colspan: 2,
             autoEl: {
                 tag: 'iframe',
                 width: '100%',
@@ -13,28 +15,48 @@ Ext.define('xlib.MiniBrowser', {
             }
         });
         
-        var navstring = Ext.create('Ext.form.field.Text', {
-            width: '100%',
-            height: 20,
+        this.navstring = Ext.create('Ext.form.field.Text', {
+            flex: 1,
             enableKeyEvents: true,
             listeners: {
                 keydown: function(field, e) {
                     if (e.getKey() == Ext.EventObject.ENTER) {
-                        var src = field.getValue();
-                        if (src.substr(0,4) !== 'http') {
-                            field.setValue('http://' + src);
-                        }
-                        iframe.getEl().dom.src = field.getValue();
+                        this.go();
                     }
                 },
                 scope: this
             }
         });
         
+        this.goButton = Ext.create('Ext.button.Button', {
+            width: 50,
+            pressed: true,
+            text: 'Найти',
+            handler: this.go,
+            scope: this
+        });
+        
         container.add(Ext.create('Ext.panel.Panel', {
-            items: [navstring, iframe]
+            layout: 'fit',
+            tbar: [
+                this.navstring, 
+                this.goButton
+            ],
+            items: [this.iframe],
+            scope: this
         }));
-            
-    }
+    },
     
+    onLoadIframe: function() {
+//        console.log('arguments: ', arguments);
+    },
+    
+    go: function() {
+//        this.iframe.getEl('iframe').on('load', this.onLoadIframe, this, {single: true}); 
+        var src = this.navstring.getValue();
+        if (src.substr(0,4) !== 'http') {
+            this.navstring.setValue('http://' + src);
+        }
+        this.iframe.getEl().dom.src = this.navstring.getValue();
+    }
 });

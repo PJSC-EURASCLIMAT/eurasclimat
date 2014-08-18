36684579e7515cf4a01169699171951e4a917895
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-08-18 04:44:02 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1459 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ modules/catalog/controllers/ElectricityLampController.php
@@ -0,0 +1,29 @@
+<?php
+
+class Catalog_ElectricityLampController extends Catalog_AbstractController
+{
+    public function init()
+    {
+        $this->_entity = 'electricity_lamp';
+        $this->_model = new Catalog_Electricity_Lamp_Model();
+        parent::init();
+    }
+
+    public function permission(Xend_Controller_Action_Helper_Acl $acl)
+    {
+        $acl->setResource(Xend_Acl_Resource_Generator::getInstance()->catalog->electricity);
+        $acl->isAllowed(Xend_Acl_Privilege::VIEW, 'get-list');
+        $acl->isAllowed(Xend_Acl_Privilege::VIEW, 'get-images');
+        $acl->isAllowed(Xend_Acl_Privilege::VIEW, 'get-fields');
+        $acl->isAllowed(Xend_Acl_Privilege::VIEW, 'get');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'add');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'update');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'delete');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'upload');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'delete-image');
+        $acl->isAllowed(Xend_Acl_Privilege::VIEW, 'get-related-services');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'add-related-services');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'edit-related-services');
+        $acl->isAllowed(Xend_Acl_Privilege::UPDATE, 'delete-related-services');
+    }
+}

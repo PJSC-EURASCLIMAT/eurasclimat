21dd2cd8d8c9cb8ae48977ced0ee474a03729352
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-08-21 16:10:53 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1461 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- httpdocs/modules/Market/view/Trade/List.js
+++ httpdocs/modules/Market/view/Trade/List.js
@@ -30,21 +30,20 @@ Ext.define('EC.Market.view.Trade.List', {
             {id: 6, name: 'Закупки Сбербанк-АСТ', url: 'http://zakupki.sbrf.sberbank-ast.ru/default.aspx'},
             {id: 7, name: 'Почта России', url: 'http://www.russianpost.ru/rp/company/ru/home/tenders/search'},
             {id: 8, name: 'ВТБ', url: 'http://www.vtb.ru/group/purchases/all/'},
-            {id: 9, name: '«Лидер-инвест»', url: 'http://www.uk.sistema.ru/pages/main/customs/index.shtml'},
+            {id: 9, name: '«Лидер-инвест»', url: 'http://www.uk.sistema.ru'},
             {id: 10, name: 'OTS tender (Метро)', url: 'http://sso.otc-tender.ru/Login.aspx'},
             {id: 11, name: 'ГУП «Московский метрополитен»', url: 'http://www.torgi-mosmetro.ru/torgi/'},
             {id: 12, name: 'Промсвязьбанк', url: 'http://www.psbank.ru/Bank/About/Tenders/Current'},
             {id: 13, name: 'МГТС', url: 'http://www.mgts.ru/partner/purchase'},
             {id: 14, name: 'МТС', url: 'http://tenders.mts.ru'},
             {id: 15, name: 'Фабрикант', url: 'http://www.fabrikant.ru'},
-            {id: 16, name: 'Гос Услуги', url: 'https://esia.gosuslugi.ru/idp/Authn/CommonLogin'},
+            {id: 16, name: 'Гос Услуги', url: 'http://esia.gosuslugi.ru'},
             {id: 17, name: 'Спецстрой', url: 'http://www.sstorg.ru'},
             {id: 18, name: 'Торговая Система "Спецстройторг"', url: 'http://www.sstorg.ru/market/?action=list_public_pdo_multilot&type=5360&status_group=sg_active'},
             {id: 19, name: 'Medsi.ru', url: 'http://medsi.ru/about/purchases/'},
             {id: 20, name: 'РЖД (ТП)', url: 'http://etzp.rzd.ru/freeccee/main'},
-            {id: 21, name: 'РЖД (сайт)', url: 'http://www.rzd.ru'},
-            {id: 22, name: 'Заказ РФ', url: 'http://zakazrf.ru'},
-            {id: 23, name: 'B2B-Center', url: 'http://www.b2b-center.ru/personal/'}   
+            {id: 21, name: 'Заказ РФ', url: 'http://zakazrf.ru'},
+            {id: 22, name: 'B2B-Center', url: 'http://www.b2b-center.ru/personal/'}   
         ]
     },
 

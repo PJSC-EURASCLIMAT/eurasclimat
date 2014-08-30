11276f4b1868459b31fd4aebe13f633f0bb333f8
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-08-30 02:51:29 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1478 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ db/migrate-2014-08-30.sql
@@ -0,0 +1,18 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE 
+    `catalog_airing_connection_types`, 
+    `catalog_airing_control_types`, 
+    `catalog_airing_groups`, 
+    `catalog_airing_implementation_types`, 
+    `catalog_airing_isolation_clases`, 
+    `catalog_airing_materials`, 
+    `catalog_airing_power_sources`, 
+    `catalog_airing_product_types`, 
+    `catalog_airing_protection_types`, 
+    `catalog_conditioners_groups`, 
+    `catalog_conditioners_implementation_types`, 
+    `catalog_conditioners_power_sources`, 
+    `catalog_conditioners_product_types`;
+
+SET FOREIGN_KEY_CHECKS=1;

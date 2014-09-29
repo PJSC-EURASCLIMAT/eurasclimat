79f2b70e6c09f47584db58c8f090ad96d9ca8e70
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-09-29 14:30:24 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1508 420e713c-08d4-4d59-a4bd-dad7fcbb932a

---
+++ httpdocs/modules/Catalog/controller/Airing.js
@@ -0,0 +1,8 @@
+Ext.define('EC.Catalog.controller.Airing', {
+    
+    extend: 'EC.Catalog.controller.ChapterAbstract',
+    
+    content: 'Раздел вентиляции. Для просмотра содержимого разверните ' +
+    		 'пункт в левом меню и выберите каталог.'
+    
+});
---
+++ httpdocs/modules/Catalog/controller/Conditioning.js
@@ -0,0 +1,8 @@
+Ext.define('EC.Catalog.controller.Conditioning', {
+    
+    extend: 'EC.Catalog.controller.ChapterAbstract',
+    
+    content: 'Раздел кондиционирования. Для просмотра содержимого разверните ' +
+             'пункт в левом меню и выберите каталог.'
+    
+});

7ab2756f1ae65b209507deaa94038e25f4d3f47a
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-09-29 14:45:24 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1509 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- modules/catalog/models/Conditioning/Blocks/Table.php
+++ modules/catalog/models/Conditioning/Blocks/Table.php
@@ -6,38 +6,7 @@ class Catalog_Conditioning_Blocks_Table extends Xend_Db_Table_Abstract
      * Table name
      * @var string
      */
-    protected $_name = 'catalog_conditioners';
+    protected $_name = 'catalog_conditioning_blocks';
 
-    protected $_nullableFields = array(
-        'code',
-        'group_id',
-        'mark_id',
-        'marking',
-        'name',
-        'product_type_id',
-        'implementation_type_id',
-        'heatingcooling_id',
-        'power_source_id',
-        'country',
-        'cooling_capacity',
-        'heating_capacity',
-        'air_consumption_min',
-        'air_consumption_max',
-        'noise_level_min',
-        'noise_level_max',
-        'dimensions',
-        'warranty',
-        'seer',
-        'scop',
-        'maxblocks',
-        'ports',
-        'sound_power_level',
-        'working_amperage',
-        'operating_amperage',
-        'max_working_amperage',
-        'factory_refrigerant_charge',
-        'price',
-        'currency_id',
-        'description'
-    );
+    protected $_nullableFields = array(s);
 }

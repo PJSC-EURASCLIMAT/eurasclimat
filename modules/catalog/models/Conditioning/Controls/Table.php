b8bbb50bc2eab6eb154d302335de5aba9f9110fb
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-10-01 21:34:26 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1533 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- modules/catalog/models/Conditioning/Controls/Table.php
+++ modules/catalog/models/Conditioning/Controls/Table.php
@@ -6,7 +6,7 @@ class Catalog_Conditioning_Controls_Table extends Xend_Db_Table_Abstract
      * Table name
      * @var string
      */
-    protected $_name = 'catalog_conditioning_blocks';
+    protected $_name = 'catalog_conditioning_controls';
 
     protected $_nullableFields = array();
 }

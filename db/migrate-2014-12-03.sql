67dca390af9e486bbce87af81a3091574e6b31ee
bvh <bvh@420e713c-08d4-4d59-a4bd-dad7fcbb932a>
2014-12-04 01:39:49 +0000

    git-svn-id: file:///var/svn-repos/eurasclimat/trunk@1585 420e713c-08d4-4d59-a4bd-dad7fcbb932a

--- db/migrate-2014-12-03.sql
+++ db/migrate-2014-12-03.sql
@@ -2,4 +2,6 @@ SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE `catalog_related_services`;
 
+DROP TABLE `crm_demoprojects`, `crm_demoprojects_discussions`, `crm_demoprojects_docs`, `crm_demoprojects_docs_versions`, `crm_demoprojects_equipment`, `crm_demoprojects_equipment_services`, `crm_demoprojects_groups`, `crm_demoprojects_members`, `crm_demoprojects_special_services`, `crm_demoprojects_special_services_expendables`;
+
 SET FOREIGN_KEY_CHECKS=1;

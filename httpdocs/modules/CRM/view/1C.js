87deb8d66f3a2d29e74be91fa878cdec3079b222
bvh <bvh.box@gmail.com>
2015-07-21 13:09:54 +0300

    Signed-off-by: bvh <bvh.box@gmail.com>

--- httpdocs/modules/CRM/view/1C.js
+++
@@ -1,9 +0,0 @@
-Ext.define('EC.CRM.view.1C', {
-
-    extend: 'Ext.panel.Panel',
-
-    layout: 'fit',
-    
-    html: '<iframe id="eventsIFrame" width="100%" height="100%" ' +
-            'src="http://95.169.190.193/eurasclimat/ru_RU/"></iframe>'
-});

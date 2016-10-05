71aed53bc1a048f08b34d24b1456f914cbb4e45f
bvh <bvh.box@gmail.com>
2016-10-05 18:30:07 +0300

    Signed-off-by: bvh <bvh.box@gmail.com>

--- httpdocs/modules/Main/view/AboutSystem/Layout.js
+++ httpdocs/modules/Main/view/AboutSystem/Layout.js
@@ -15,9 +15,9 @@ Ext.define('EC.Main.view.AboutSystem.Layout', {
         border: '0 1 0 0',
         width: 200
     }, {
-        xtype: 'panel',
+      xtype: 'panel',
         type: 'preview',
-        border: '0 0 0 1',
+        border: false,
         bodyPadding: 5,
         autoScroll: true,
         bodyStyle: 'font-size:11px;',
@@ -28,6 +28,24 @@ Ext.define('EC.Main.view.AboutSystem.Layout', {
         layout: 'fit',
         region: 'center',
         dockedItems: [{
+            xtype: 'panel',
+            dock: 'top',
+            height: 140,
+            padding: 10,
+            border: false,
+            layout: 'border',
+            items: [{
+                xtype: 'panel',
+                region: 'west',
+                border: false,
+                html: '<img src="/images/logo.png" style="padding-left:20px;">'
+            },{
+                xtype: 'panel',
+                region: 'east',
+                border: false,
+                html: '<img src="/images/rekvizits.png" height="120" style="padding-right:30px;">'
+            }]
+        }, {
             xtype: 'toolbar',
             dock: 'bottom', 
             hidden: !acl.isUpdate('aboutsystem'),
